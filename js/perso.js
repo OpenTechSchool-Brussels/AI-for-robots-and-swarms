785231280acadeb968075e3d29c9fce36f79c7ef
Roman Miletitch <roman.miletitch@gmail.com>
2014-11-27 20:11:24 +0100

    Update perso.js

--- js/perso.js
+++ js/perso.js
@@ -12,14 +12,16 @@ var addEvent = function(elem, type, eventHandle) {
 
 addEvent(window, "resize", function() {
                         
-                        alert("d width =" + window.innerWidth);
+                          //alert("d width =" + window.innerWidth);
                           if(window.innerWidth > 702) {
                             document.getElementById(p_menu).style.width ="100%";
                             document.getElementById(p_content).style.width = "100%";
                           } else {
+                            alert(small)
                             document.getElementById(p_menu).style.width = "23%";
+                            alert(document.getElementById(p_menu).style);
                             document.getElementById(p_content).style.width = "69%";
                           }
-                          }
+                        }
 
 );
