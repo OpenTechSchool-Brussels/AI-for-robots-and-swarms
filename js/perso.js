a8dfccbadbbdaa48ffbc505fd570f1a7ff3c179b
Roman Miletitch <roman.miletitch@gmail.com>
2014-11-27 20:04:42 +0100

    Update perso.js

--- js/perso.js
+++ js/perso.js
@@ -15,7 +15,7 @@ addEvent(window, "resize", function() {
                         alert(document.width);
                           if(document.width > 702) {
                             document.getElementById(p_menu).style.width ="100%";
-                            document.getElementById(p_conten)t.style.width = "100%";
+                            document.getElementById(p_content).style.width = "100%";
                           } else {
                             document.getElementById(p_menu).style.width = "23%";
                             document.getElementById(p_content).style.width = "69%";
